1a35b5b2f4c88d96ffa4dd8414c9b08359372315
username <username@email.com>
2019-11-19 10:38:45 +0800

    用户注册接口设计和定义

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -5,4 +5,6 @@ from django.views.generic import View
 class RegisterView(View):
     def get(self, request):
         return render(request, 'register.html')
+    def post(self, request):
+        pass
 

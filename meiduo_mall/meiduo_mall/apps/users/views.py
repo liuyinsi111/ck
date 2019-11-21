2e90629a30cc6da6b391c43ebbc36c5d3502b03d
username <username@email.com>
2019-11-21 09:39:38 +0800

    最最后的修改

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -30,15 +30,7 @@ class RegisterView(View):
         # 判断手机号是否合法
         if not re.match(r'^1[3-9]\d{9}$', mobile):
             return http.HttpResponseForbidden('请输入正确的手机号码')
-        #TODO：短信验证码的验证代码后期补充
-        user = User.objects.create(
-            username=username,
-            password=password,
-            mobile=mobile
-        )
-        # user.set_password(password)
-        # user.save()
-        # user.check_password()
+
         user = User.objects.create_user(username=username, password=password, mobile=mobile)
         login(request, user)
         return http.HttpResponse('注册成功即代表登陆成功')

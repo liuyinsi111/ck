d78e0eef352dd943b1edaba2ed8347da9b98d9a5
username <username@email.com>
2019-11-21 10:30:04 +0800

    发短信

--- meiduo_mall/meiduo_mall/apps/verification/views.py
+++ meiduo_mall/meiduo_mall/apps/verification/views.py
@@ -5,6 +5,11 @@ from django.views import View
 from django import http
 from meiduo_mall.libs.captcha.captcha import captcha
 from meiduo_mall.libs.response_code import RETCODE
+from meiduo_mall.libs.yuntongxun.sms import CCP
+from random import randint
+import logging
+logger = logging.getLogger('django')
+
 
 
 class ImageCodeView(View):
@@ -29,3 +34,17 @@ class SMSCodeView(View):
         image_code_server = image_code_server_bytes.decode()
         if image_code_client.lower() != image_code_server.lower():
             return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图形验证码填写错误'})
+
+        # CCP().send_template_sms('接收短信的手机号', ['验证码', '提示用户的过期时间：单妙分钟'], 1)
+
+        sms_code = '%06d'% randint(0,999999)
+        logger.info(sms_code)
+        #except里面用error（）
+        #随机生成6位数字，为了方便测试将验证码发到控制台
+        CCP().send_template_sms('接收短信的手机号', ['验证码', '提示用户的过期时间：单秒分钟'], 1)
+        redis_conn.setex('sms_%s' % mobile, 300, sms_code)
+        #保存验证码到redis，方便以后验证
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK'})
+    #ok代表字符串0，
+
+

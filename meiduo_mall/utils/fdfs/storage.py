82fa89e9c70a313a3c807a747fc95c3458261062
韦伟 <weiwei@itcast.cn>
2019-12-09 14:38:19 +0800

    mis后台管理项目初始化

---
+++ meiduo_mall/utils/fdfs/storage.py
@@ -0,0 +1,15 @@
+from django.core.files.storage import Storage
+from django.conf import settings
+from django.utils import timezone
+from fdfs_client.client import Fdfs_client
+from rest_framework import serializers
+
+class FdfsStorage(Storage):
+    def _open(self, name, mode='rb'):
+        pass
+
+    def _save(self, name, content, max_length=None):
+       pass
+
+    def url(self, name):
+        return settings.FDFS_URL + name

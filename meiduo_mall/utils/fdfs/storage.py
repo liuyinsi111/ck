2aaef80f51d7f5309133e2b4b249e5728ed6c947
liuyinsi111 <1446171351@qq.com>
2019-12-14 18:14:18 +0800

    自定义fdfs文件上传函数custom_upload_handler,在storage.py中增加exists方法告诉django存储后端不需要检验是否重复存储，并且在storage.py中的save方法中自定义存储逻辑，返回存储字节id

--- meiduo_mall/utils/fdfs/storage.py
+++ meiduo_mall/utils/fdfs/storage.py
@@ -3,13 +3,17 @@ from django.conf import settings
 from django.utils import timezone
 from fdfs_client.client import Fdfs_client
 from rest_framework import serializers
-
+from meiduo_mall.utils.fdfs.custom_upload_handler import custom_upload_by_buffer
 class FdfsStorage(Storage):
     def _open(self, name, mode='rb'):
         pass
 
     def _save(self, name, content, max_length=None):
-       pass
+        file_bytes = content.read()  # 文件的字节数据
+        file_id = custom_upload_by_buffer(file_bytes)
+        return file_id
 
     def url(self, name):
         return settings.FDFS_URL + name
+    def exists(self, name):
+        return False
